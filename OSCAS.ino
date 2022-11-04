fd2fd20b53dd4acb007081b65602bd6f3959e077
Advait Muley <114322929+CodeBuzz-ML@users.noreply.github.com>
2022-11-04 22:28:34 +0530

    Here's the new code

---
+++ OSCAS.ino
@@ -0,0 +1,136 @@
+/* This Code is written by CodeBuzz (Advait Muley)
+   http://codebuzz.ml is my blog/website
+
+   Author: - Advait Muley 
+   Language: - C++ (Arduino)
+
+   Thank You!
+*/
+
+//Including Essential Libraries
+  #include <LiquidCrystal_I2C.h>
+  #include <Wire.h>
+  #include <HCSR04.h>
+  #include <Servo.h>
+
+// Define Ultrasonic Sensor Pin
+  #define TrigPin  8
+  #define EchoPin  7
+
+//Define custom LCD characters
+
+  //Middle Pointer
+    byte Mid[8] = {B00100, B00100, B00100, B00100, B00100, B11111, B11111, B00100};
+  //Left Pointer
+    byte Left[8] = {B00000, B00000, B10000, B01000, B00100, B11111, B11111, B00100};
+  //Right Pointer
+    byte Right[8] = {B00000, B00000, B00001, B00010, B00100, B11111, B11111, B00100}; 
+    
+LiquidCrystal_I2C lcd(0x27,16,2); //LCD Display Initialization
+
+
+// Variable to store distance
+  //When the servo is at 90° position (Front)
+    int Front_Distance = 0;
+  //When the servo is at 135° position (Left)
+    int Left_Distance = 0;
+  //When the servo is at 45° position (Right) 
+    int Right_Distance = 0;
+
+//Initialize the ultrasonic sensor
+  UltraSonicDistanceSensor distanceSensor(TrigPin, EchoPin);  
+
+//Creating an object for the servo motor named servo
+  Servo servo;
+
+//Creating a variable for storing the servo position
+  int pos = 0;
+
+void setup() 
+{
+  lcd.begin(); //Beginning the LCD display
+  lcd.backlight(); //Turning on the LCD display backlight
+  servo.attach(A0); //Attaching the servo motor object to the pin Analog 0 (A0)
+  int pos = 90; //Changing the value of the variable pos to 90°
+  servo.write(pos); //Writing the value of the variable pos (90°) to the servo motor
+  delay(1000);  //Adding a delay of 1 second (1000 milliseconds) for smooth transition
+}
+
+void loop() 
+{
+  //Creating custom characters with the bytes we made before on lines 23, 25 and 27 respectively
+  lcd.createChar(0,Mid); //Middle symbol
+  lcd.createChar(1,Left); //Left symbol
+  lcd.createChar(2,Right); //Right symbol
+
+  //Creating a forever loop to execute the custom function MidProcess() that is created on line 74
+  while(true)
+  {     
+    MidProcess(); 
+  }     
+}
+
+
+void MidProcess()
+{
+  /* This set of instructions controls all the modules when the servo is pointing on right*/
+  servo.write(45);   // Right Position 
+  Right_Distance = distanceSensor.measureDistanceCm();
+  lcd.clear();  //Clear the LCD screen just in case anything is alredy present
+  lcd.setCursor(0,0); //Set the cursor on column 0 and row 0 to write the next character on the LCD display
+  lcd.print(char(2)); //Print the custom symbol of Right Position
+  lcd.setCursor(3,0); //Leave one character space 
+  lcd.print("Distance"); //Print the string "Distance"
+  lcd.setCursor(0,1); //Shifting to the next line
+  lcd.print(Right_Distance);  //Print the distance of the object on the right in CM
+  lcd.setCursor(14,1);  //Move to the end of the second line
+  lcd.print("CM");  //Print the string "CM"
+  delay(2000); //A two second delay to make the LCD readable
+  
+
+  /* This set of instructions controls all the modules when the servo is pointing on front*/     
+  servo.write(90);  // Front Positon 
+  Front_Distance = distanceSensor.measureDistanceCm();
+  lcd.clear();  //Clear the LCD screen just in case anything is alredy present
+  lcd.setCursor(0,0); //Set the cursor on column 0 and row 0 to write the next character on the LCD display
+  lcd.print(char(0)); //Print the custom symbol of Front Position
+  lcd.setCursor(3,0); //Leave one character space 
+  lcd.print("Distance");  //Print the string "Distance"
+  lcd.setCursor(0,1); //Shifting to the next line
+  lcd.print(Front_Distance);  //Print the distance of the object on the front in CM
+  lcd.setCursor(14,1);  //Move to the end of the second line
+  lcd.print("CM");  //Print the string "CM"
+  delay(2000);  //A two second delay to make the LCD readable
+
+
+             
+  servo.write(135); // Left position of servo
+  Left_Distance = distanceSensor.measureDistanceCm();  
+  lcd.clear();  //Clear the LCD screen just in case anything is alredy present
+  lcd.setCursor(0,0); //Set the cursor on column 0 and row 0 to write the next character on the LCD display
+  lcd.print(char(1)); //Print the custom symbol of Left Position
+  lcd.setCursor(3,0); //Leave one character space
+  lcd.print("Distance");  //Print the string "Distance"
+  lcd.setCursor(0,1); //Shifting to the next line
+  lcd.print(Left_Distance); //Print the distance of the object on the front in CM
+  lcd.setCursor(14,1);  //Move to the end of the second line
+  lcd.print("CM");  //Print the string "CM"
+  delay(2000);  //A two second delay to make the LCD readable       
+
+    
+  servo.write(90);  // back to front
+  Front_Distance = distanceSensor.measureDistanceCm();
+  lcd.clear();  //Clear the LCD screen just in case anything is alredy present
+  lcd.setCursor(0,0); //Set the cursor on column 0 and row 0 to write the next character on the LCD display
+  lcd.print(char(0)); //Print the custom symbol of Front Position
+  lcd.setCursor(3,0); //Leave one character space 
+  lcd.print("Distance");  //Shifting to the next line
+  lcd.setCursor(0,1); //Print the distance of the object on the front in CM
+  lcd.print(Front_Distance);  //Print the distance of the object on the front in CM
+  lcd.setCursor(14,1);  //Move to the end of the second line
+  lcd.print("CM");  //Print the string "CM"
+  delay(2000);  //A two second delay to make the LCD readable  
+}//Exit the code and loop it again
+
+
+ 
